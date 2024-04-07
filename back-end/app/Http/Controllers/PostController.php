876a225bc8202e92b8594fcd7438ff2bfd6e0314
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-07 12:28:31 +0300

    Posts backend

---
+++ back-end/app/Http/Controllers/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    public function createPost(Request $request) {
+        $this->validate($request, [
+            'caption' =>'required|string',
+            'image' =>'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $image = $request->file('image');
+        $imageName = time(). '.'. $image->extension();
+        $image->move(public_path('images'), $imageName);
+
+        $post = new Post();
+        $post->user_id = Auth::id();
+        $post->caption = $request->caption;
+        $post->image = $imageName;
+        $post->save();
+
+        return response()->json([
+            'message' => 'Post created successfully',
+            'post' => $post,
+        ], 201);
+    }
+
+    public function getPosts() {
+        $posts = Post::with('user')->latest()->get();
+
+        return response()->json([
+            'posts' => $posts,
+        ], 200);
+    }
+}
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,9 @@ Route::controller(AuthController::class)->group(function () {
 });
 
 Route::post('user/update', [UserController::class, 'updateUser']);
+
+Route::post('posts', [PostController::class, 'createPost']);
+Route::get('posts', [PostController::class, 'getPosts']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
