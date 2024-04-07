76c5df355623d58066fff23099e260f1106f5858
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-07 23:57:49 +0300

    Frontend cleaning

---
+++ front-end/src/core/tools/apiRequest.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000/api";
+
+export const sendRequest = async (method, route, body) => {
+  const response = await axios.request({
+    method: method,
+    url: route,
+    data: body,
+    headers: {
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+      Accept: "application/json",
+    },
+  });
+
+  return response;
+};
