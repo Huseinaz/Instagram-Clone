2d6632f9014ed0bb0ab755dfa2979cbe58125a6a
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-09 21:50:53 +0300

    Home Page

--- front-end/src/components/Home/Home.jsx
+++ front-end/src/components/Home/Home.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './index.css';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default Home
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/posts');
+      setPosts(response.data.posts);
+      console.log(response);
+    } catch (error) {
+      console.error('Error fetching posts:', error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className='home-page'>
+      <nav className='navbar'>
+        {/* Your Navbar Content Goes Here */}
+        Navbar
+      </nav>
+      <div className='posts-list'>
+        {posts.map((post) => (
+          <div key={post.id} className='post'>
+            <div className='post-header'>
+              <img
+                className='user-avatar'
+                src={`http://localhost:8000/images/${post.user.profile_picture}`}
+                alt={post.user.name}
+              />
+              <p className='username'>{post.user.name}</p>
+            </div>
+            <img className='post-image' src={`http://localhost:8000/images/${post.image}`} alt={post.caption} />
+            <div className='post-details'>
+              <p className='username'>{post.user.name}</p>
+              <p className='post-caption'>{post.caption}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
